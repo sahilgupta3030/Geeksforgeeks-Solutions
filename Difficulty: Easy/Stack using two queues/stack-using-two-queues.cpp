15d85256d97d81c14693faacbea2a4cc03d0717d
SAHIL GUPTA <sahilgupta3030@gmail.com>
2024-08-05 22:41:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Stack using two queues/stack-using-two-queues.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+class QueueStack{
+private:
+    queue<int> q1;
+    queue<int> q2;
+public:
+    void push(int);
+    int pop();
+};
+
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        QueueStack *qs = new QueueStack();
+
+        int Q;
+        cin>>Q;
+        while(Q--){
+        int QueryType=0;
+        cin>>QueryType;
+        if(QueryType==1)
+        {
+            int a;
+            cin>>a;
+            qs->push(a);
+        }else if(QueryType==2){
+            cout<<qs->pop()<<" ";
+
+        }
+        }
+        cout<<endl;
+    }
+}
+
+// } Driver Code Ends
+
+
+void QueueStack :: push(int x)
+{
+       q1.push(x);
+}
+
+int QueueStack :: pop()
+{
+    if(q1.empty()){
+           return -1;
+       }
+       
+       int n=q1.size();
+       
+       for(int i=0;i<n-1;i++){
+           q1.push(q1.front());
+           q1.pop();
+       }
+       
+       int ans=q1.front();
+       
+       
+       
+       q1.pop();
+       return ans;
+}
